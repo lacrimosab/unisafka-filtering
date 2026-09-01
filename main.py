df7ad784370dbe8545ff6b02ad80758a9de114aa
Beka <fl4mezleet@gmail.com>
2026-09-01 20:21:25 +0300

    tested the allergens and diet codes implementation

--- main.py
+++ main.py
@@ -3,5 +3,10 @@ from src.providers.sodexo import get_hertsi_meal
 meals = get_hertsi_meal("2026-09-01")
 
 for meal in meals:
-    print(meal)
+    diets = ", ".join(sorted(meal.diets)) or "No specific diets"
+    allergens = ", ".join(sorted(meal.allergens)) or "No allergens"
+
+    print(f"\n{meal.name}")
+    print(f"  Diet codes: {diets}")
+    print(f"  Allergens: {allergens}")
 
--- src/providers/sodexo.py
+++ src/providers/sodexo.py
@@ -12,13 +12,31 @@ def get_hertsi_meal(date: str) -> list[Meal]:
     response.raise_for_status()
 
     data = response.json()
-    print(next(iter(data["courses"].values())))
 
     meals = []
     for course in data["courses"].values():
+
+        dietcodes = course.get("dietcodes") or ""
+        additional_diet_info = course.get("additionalDietInfo") or {}
+        allergen_text = additional_diet_info.get("allergens_en") or ""
+
+        diets = set()
+        for code in dietcodes.split(","):
+            cleaned_code = code.strip()
+            if cleaned_code:
+                diets.add(cleaned_code)
+                
+        allergens = set()
+        for allergen in allergen_text.split(","):
+            cleaned_allergen = allergen.strip()
+            if cleaned_allergen:
+                allergens.add(cleaned_allergen)
+
         meal = Meal(
             restaurant="Hertsi",
             name=course["title_en"],
+            diets=diets,
+            allergens=allergens,
         )
 
         meals.append(meal)
