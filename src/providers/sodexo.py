f673b6a1adf53e930887a84a670807a4942eedb0
Beka <fl4mezleet@gmail.com>
2026-09-01 18:06:32 +0300

    added sodexo json reading, fixed python extensions

---
+++ src/providers/sodexo.py
@@ -0,0 +1,25 @@
+import requests
+
+from src.models import Meal
+
+def get_hertsi_meal(date: str) -> list[Meal]:
+    url = (
+        "https://www.sodexo.fi/"
+        f"ruokalistat/output/daily_json/111/{date}"
+    )
+
+    response = requests.get(url, timeout=10)
+    response.raise_for_status()
+
+    data = response.json()
+
+    meals = []
+    for course in data["courses"].values():
+        meal = Meal(
+            restaurant="Hertsi",
+            name=course["title_fi"],
+        )
+
+        meals.append(meal)
+
+    return meals
