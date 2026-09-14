c0907ae918546150648f6d0fb5205dc149ca4c27
Beka <fl4mezleet@gmail.com>
2026-09-15 00:06:24 +0300

    added initial parsing of days of the week from compass website

--- src/providers/compass.py
+++ src/providers/compass.py
@@ -1,3 +1,4 @@
+from bs4 import BeautifulSoup
 import requests
 
 REAKTORI_URL = (
@@ -15,3 +16,15 @@ def fetch_reaktori_page() -> str:
     response.raise_for_status()
 
     return response.text
+
+def reaktori_day_headings() -> list[str]:
+    html = fetch_reaktori_page()
+    soup = BeautifulSoup(html, "html.parser")
+    headings = soup.find_all("h3")
+
+    day_headings = []
+    for heading in headings:
+        text = heading.get_text(" ", strip=True)
+        day_headings.append(text)
+
+    return day_headings
