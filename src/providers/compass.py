beb018fdcec9573b28fd1d2ae6204f2be2444a39
Beka <fl4mezleet@gmail.com>
2026-09-15 00:27:15 +0300

    replaced URL to eng, parsed also the food categories

--- src/providers/compass.py
+++ src/providers/compass.py
@@ -1,9 +1,12 @@
+from datetime import date
+
 from bs4 import BeautifulSoup
 import requests
 
+
 REAKTORI_URL = (
-    "https://www.compass-group.fi/"
-    "ravintolat-ja-ruokalistat/foodco/"
+    "https://www.compass-group.fi/en/"
+    "ravintolat-ja-ruokalistat/food--co/"
     "kaupungit/tampere/reaktori/"
 )
 
@@ -17,14 +20,35 @@ def fetch_reaktori_page() -> str:
 
     return response.text
 
-def reaktori_day_headings() -> list[str]:
+def get_reaktori_meal_headings(selected_date: date) -> list[str]:
     html = fetch_reaktori_page()
     soup = BeautifulSoup(html, "html.parser")
-    headings = soup.find_all("h3")
 
-    day_headings = []
-    for heading in headings:
-        text = heading.get_text(" ", strip=True)
-        day_headings.append(text)
+    date_text = (
+        f"{selected_date.day}."
+        f"{selected_date.month}."
+        f"{selected_date.year}"
+    )
 
-    return day_headings
+    selected_day_heading = None
+
+    for heading in soup.find_all("h3"):
+        heading_text = heading.get_text(" ", strip = True)
+
+        if date_text in heading_text:
+            selected_day_heading = heading
+            break   
+
+    if selected_day_heading is None:
+        return []
+
+    meal_headings = []
+    
+    # h3 is day of the week, h4 is the meal category
+    for heading in selected_day_heading.find_all_next(["h3", "h4"]):
+        if heading.name == "h3":
+            break
+
+        meal_headings.append(heading.get_text(" ", strip = True))
+
+    return meal_headings
