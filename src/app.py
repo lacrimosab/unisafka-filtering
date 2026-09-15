a5c1317b6711910696c6fc68ee1fb86ab88bff34
Beka <fl4mezleet@gmail.com>
2026-09-16 01:43:48 +0300

    backend code added for the ribbon calendar

--- src/app.py
+++ src/app.py
@@ -2,12 +2,10 @@ from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
-from datetime import date
+from datetime import date, timedelta
 from src.providers.sodexo import get_hertsi_meal
 from src.providers.compass import get_reaktori_meals
 from src.filtering import filter_meals
-
-
 
 app = FastAPI()
 app.mount(
@@ -30,6 +28,12 @@ def home(
     selected_date = menu_date or date.today().isoformat()
     requested_date = date.fromisoformat(selected_date)
 
+    week_start = requested_date - timedelta(days=requested_date.weekday())
+    week_dates = [
+        week_start + timedelta(days=day_offset)
+        for day_offset in range(6)
+    ]
+
     hertsi_meals = get_hertsi_meal(selected_date)
     reaktori_meals = get_reaktori_meals(requested_date)
 
@@ -47,6 +51,7 @@ def home(
         name="index.html",
         context={"meals": meals,
                 "selected_date": selected_date,
+                "week_dates": week_dates,
                 "vegan_only": vegan_only, # added to keep checkbox even after page reload
                 "gluten_free_only": gluten_free_only,
                 "lactose_free_only": lactose_free_only,
