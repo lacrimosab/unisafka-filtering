f1d03c1ad725b344b49cc0476442f537dfd9cad7
Beka <fl4mezleet@gmail.com>
2026-09-16 03:08:20 +0300

    built filter query parameters on checkbox change

--- static/app.js
+++ static/app.js
@@ -7,16 +7,22 @@ const filterCheckboxes = filterForm.querySelectorAll(
 
 // function that runs after checkbox changes
 function handleFilterChange(event) {
-    // event.target is the specific checkbox the user clicked
-    const changedCheckbox = event.target;
-
-    console.log(
-        changedCheckbox.name,
-        changedCheckbox.checked
+    const formData = new FormData(filterForm)
+    const parameters = new URLSearchParams(formData);
+    
+    const selectedDateButton = filterForm.querySelector(
+        ".date-option--selected"
     );
+    
+    parameters.set(
+        "menu_date",
+        selectedDateButton.value
+    )
+    
+    console.log(parameters.toString());
 }
 
 // visits all three checkboxes; addEventListener connects each checkbox to the function
 filterCheckboxes.forEach((checkbox) => {
     checkbox.addEventListener("change", handleFilterChange);
-})
+});
