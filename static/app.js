2bbf9aad49cb80ddb0a585e8079dcdcb7070d863
Beka <fl4mezleet@gmail.com>
2026-09-16 03:02:40 +0300

    logging changes of filter tickboxes with js

--- static/app.js
+++ static/app.js
@@ -1,2 +1,22 @@
 const filterForm = document.querySelector(".date-form");
 const mealResults = document.querySelector("#meal-results");
+
+const filterCheckboxes = filterForm.querySelectorAll(
+    'input[type="checkbox"]'
+);
+
+// function that runs after checkbox changes
+function handleFilterChange(event) {
+    // event.target is the specific checkbox the user clicked
+    const changedCheckbox = event.target;
+
+    console.log(
+        changedCheckbox.name,
+        changedCheckbox.checked
+    );
+}
+
+// visits all three checkboxes; addEventListener connects each checkbox to the function
+filterCheckboxes.forEach((checkbox) => {
+    checkbox.addEventListener("change", handleFilterChange);
+})
